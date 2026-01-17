af4c2af7b5afa467aa4a3e8fe83696a896127168
Wojtmic <mail@wojtmic.dev>
2026-01-17 12:57:51 +0100

    add xdg compliance

--- lazurich/core/paths.py
+++ lazurich/core/paths.py
@@ -1,7 +1,13 @@
 from pathlib import Path
 import os
 
-STORAGE_ROOT = Path('~/.local/share/lazurich').expanduser()
+xdg_storage  = os.environ.get('XDG_DATA_HOME', '~/.local/share')
+xdg_state    = os.environ.get('XDG_STATE_HOME', '~/.local/state')
+xdg_config   = os.environ.get('XDG_CONFIG_HOME', '~/.config')
+
+STORAGE_ROOT = Path(xdg_storage).expanduser() / 'lazurich'
+LOG_ROOT     = Path(xdg_state)  .expanduser() / 'lazurich'
+CONFIG_ROOT  = Path(xdg_config) .expanduser() / 'lazurich'
 SOCKET       = Path(f'/run/user/{os.getuid()}/lazurich.sock')
 
 # Storage subdirs
@@ -9,15 +15,14 @@ INSTANCES = STORAGE_ROOT / 'instances'
 DATA      = STORAGE_ROOT / 'data'
 STORE     = STORAGE_ROOT / 'store'
 ASSETS    = STORAGE_ROOT / 'assets'
-NATIVES   = STORAGE_ROOT / 'natives'
 JARS      = STORAGE_ROOT / 'jars'
 TEMPLATES = STORAGE_ROOT / 'templates'
 EXTENSIONS= STORAGE_ROOT / 'extensions'
-LOGS      = STORAGE_ROOT / 'logs'
 
 # Data files
-CONFIG    = Path('~/.config/lazurich.json').expanduser()
-ACCOUNTS  = DATA / 'accounts.json'
+CONFIG    = CONFIG_ROOT / 'config.json'
+ACCOUNTS  = CONFIG_ROOT / 'accounts.json'
+INSTANCE  = CONFIG_ROOT / 'instances.json'
 TOKENS    = DATA / 'msal_token_cache-DO-NOT-SEND-TO-ANYONE.bin'
 
 # Store structure
@@ -28,6 +33,6 @@ MD5       = STORE / 'md5'
 
 def create_paths():
     all_dirs = [INSTANCES, DATA, STORE, ASSETS, NATIVES, JARS,
-                TEMPLATES, EXTENSIONS, LOGS, SHA256, SHA512, SHA1, MD5]
+                TEMPLATES, EXTENSIONS, SHA256, SHA512, SHA1, MD5, CONFIG_ROOT, LOG_ROOT]
     for directory in all_dirs:
         directory.mkdir(parents=True, exist_ok=True)
