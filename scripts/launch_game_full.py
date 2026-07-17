af738401e7ff8ff2c774f7182e71cb4f85018a06
Wojtmic <mail@wojtmic.dev>
2026-07-17 22:27:22 +0200

    fix(scripts): launch game full event loop

--- scripts/launch_game_full.py
+++ scripts/launch_game_full.py
@@ -9,26 +9,21 @@ from lazurich.core.models.general import Instance, ModloaderEnum
 from lazurich.core.natives import download_natives, extract_natives
 from lazurich.core.paths import INSTANCES
 
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(download_version_assets('26.1.2'))
-    asyncio.run(download_version_manifest('26.1.2'))
-
-    asyncio.run(download_natives('26.1.2'))
+async def main():
+    await download_version_assets('26.1.2')
+    await download_version_manifest('26.1.2')
+    await download_natives('26.1.2')
     extract_natives('26.1.2')
+    await download_version_jar('26.1.2')
 
-    asyncio.run(download_version_jar("26.1.2"))
-
-    inst = Instance(
-        name='epic instnace',
-        version='26.1.2',
-        modloader=ModloaderEnum.VANILLA,
-        modloader_version=''
-    )
-    import asyncio
-    id = asyncio.run(create_instance(inst))
-    fill_instance(id)
+    inst = Instance(name='epic instnace', version='26.1.2', modloader=ModloaderEnum.VANILLA, modloader_version='')
+    instance_id = await create_instance(inst)
+    fill_instance(instance_id)
 
     msa = get_msa_token()
-    prof, token = asyncio.run(do_full_auth(msa))
-    launch_game('26.1.2', INSTANCES / id / '.minecraft', prof, token)
+    prof, token = await do_full_auth(msa)
+    launch_game('26.1.2', INSTANCES / instance_id / '.minecraft', prof, token)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
