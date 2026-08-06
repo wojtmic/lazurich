55816f2f542a92897f3a8bd5060f067d3a9520e2
Wojtmic <mail@wojtmic.dev>
2026-08-06 19:49:06 +0200

    feat(gui): expand theming further

--- lazurich/gui/__main__.py
+++ lazurich/gui/__main__.py
@@ -176,8 +176,8 @@ class App(slint.load_file(Path(__file__).parent / 'layouts' / 'main.slint').AppW
         self._debug_window.show()
 
 
-async def load_app(app, theme_success=0):
-    if theme_success != 0:
+async def load_app(app, theme_options: dict, theme_success: int):
+    if theme_success == 1:
         await app.display_error('Your theme has a version lower than the current standard! To avoid issues, the default theme has been loaded. Contact your theme\'s author and if you are, update it according to the docs!')
 
     manifest = await read_manifest()
@@ -221,13 +221,13 @@ def main():
             return
 
     app = App()
-    theme_success = apply_theme(app, Path(__file__).parent / "theme")
+    options, theme_success = apply_theme(app, Path(__file__).parent / "theme")
     app.dev = os.environ.get('LAZURICH_DEV', 'false').lower() == 'true' or '--dev' in sys.argv
     app.git_hash = get_git_hash()
     # app.run()
 
     app.show()
-    slint.run_event_loop(load_app(app, theme_success))
+    slint.run_event_loop(load_app(app, options, theme_success))
     app.hide()
 
     if get_os_name() != 'windows':
--- lazurich/gui/theme.py
+++ lazurich/gui/theme.py
@@ -5,12 +5,25 @@ import slint
 ICON_NAMES = ["dashboard", "list", "settings", "skin"]
 CURRENT_VERSION = 1
 
-def apply_theme(app, theme_dir: Path) -> int:
+def apply_theme(app, theme_dir: Path) -> tuple[dict[str, str], int]:
     code = 0
     palette = json.loads((theme_dir / "palette.json").read_text())
     if palette.get('version', 0) < CURRENT_VERSION:
+        print('TRIGGER ON FUCKING THIS HERE')
+        print(palette.get('version', 0))
         code = 1
         theme_dir = Path(__file__).parent / 'theme'
+        palette = json.loads((theme_dir / "palette.json").read_text())
+    print(palette.get('version', 0))
+
+    options = json.loads((theme_dir / "options.json").read_text())
+    if options.get('version', 0) < CURRENT_VERSION:
+        print('TRIGGER ON THAT FUCKING OTHER ONE')
+        print(options.get('version', 0))
+        code = 1
+        theme_dir = Path(__file__).parent / 'theme'
+        options = json.loads((theme_dir / "options.json").read_text())
+    print(options.get('version', 0))
 
     for key, hex_value in palette.items():
         if key == 'version': continue
@@ -21,4 +34,5 @@ def apply_theme(app, theme_dir: Path) -> int:
         if icon_path.exists():
             setattr(app.Theme, f"{name}_icon", slint.Image.load_from_path(str(icon_path)))
 
-    return code
+    print(code)
+    return options, code
